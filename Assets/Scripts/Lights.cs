32f2351c44de2eb042109ca21d484b8ea0545b99
maadamoski <maadamoski@gmail.com>
2018-01-27 04:24:06 -0200

    scripts panel e light

---
+++ Assets/Scripts/Lights.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lights : MonoBehaviour {
+    private bool light = true;
+    private SpriteRenderer spr_renderer;
+
+    Config config = new Config();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(config.lightIsOn == true)
+        {
+            Color c = new Color(spr_renderer.color.r, spr_renderer.color.g, spr_renderer.color.b, 1f);
+            spr_renderer.color = c;
+            light = true;
+        }
+        else if(config.lightIsOn == false)
+        {
+            Color c = new Color(spr_renderer.color.r, spr_renderer.color.g, spr_renderer.color.b, 0.03f);
+            spr_renderer.color = c;
+            light = false;
+
+        }
+		
+	}
+}
---
+++ Assets/Scripts/Panel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Panel : MonoBehaviour {
+
+    private bool key_panel = true;
+    Config config = new Config();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(key_panel == true)
+        {
+            config.lightIsOn = true;
+        }
+        else if (key_panel == false)
+        {
+            config.lightIsOn = false;
+
+          
+        }
+		
+	}
+}
