a36c866ea4099351b1a9b8e846002a0f0f296e78
Luis Boch <luis.c.boch@gmail.com>
2018-01-28 10:50:15 -0200

    Adicionando sons

--- Assets/Animations/SoldierAnimatorController.cs
+++ Assets/Animations/SoldierAnimatorController.cs
@@ -2,12 +2,21 @@ using UnityEngine;
 
 public class SoldierAnimatorController : MonoBehaviour {
     private Animator animator;
+    private AudioSource walkSource;
 
     void Start() {
         this.animator = GetComponent<Animator>();
+        this.walkSource = GetComponent<AudioSource>();
     }
 
     public void setForward(float forward) {
         this.animator.SetFloat("Forward", forward);
+        if(walkSource){
+            if(forward > 0.2f){
+                walkSource.enabled = true;
+            } else {
+                walkSource.enabled = false;
+            }
+        }
     }
 }
