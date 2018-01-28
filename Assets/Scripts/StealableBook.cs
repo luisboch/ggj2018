4a86f157cbbe2d811db6319ad6cc7748e8c3ea86
Andre Gavazzoni <andre.gavazzoni@gmail.com>
2018-01-28 01:59:46 +0000

    Book Script

--- Assets/Scripts/StealableBook.cs
+++ Assets/Scripts/StealableBook.cs
@@ -2,15 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StealableBook : MonoBehaviour {
+public class StealableBook : MonoBehaviour
+{
 
+	public float Radius = 0.3f;
+	
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.green;
+		Gizmos.DrawWireSphere(transform.position, Radius);
+	}
+
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.GetButtonDown("X360_A01"))
+		{
+			Collider[] col = Physics.OverlapSphere(transform.position, Radius);
+			foreach (Collider c in col)
+			{
+				if (c.tag.Equals("Player"))
+				{
+					//Logica de pegar o livro de info
+				}
+			}
+		}
 	}
 }
